af0125d396f40047f159414c51763aad71ffdf71
Kyle Conibear <kyle@gamepill.com>
2020-11-17 20:51:37 -0500

    Adding character with locomotion

---
+++ Unity/Assets/Project/Character.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField] private Animator animator = null;
+
+    [SerializeField] new private Rigidbody rigidbody = null;
+
+    [Range(5, 25)]
+    [SerializeField] private float baseForce = 6;
+
+    protected void Move(Vector3 direction)
+    {
+        this.rigidbody.velocity = direction * this.baseForce;
+        this.animator.SetFloat("xVel", direction.x);
+        this.animator.SetFloat("zVel", direction.z);
+    }
+}
---
+++ Unity/Assets/Project/Player.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{
+    private void FixedUpdate()
+    {
+        base.Move(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")));
+    }
+}
