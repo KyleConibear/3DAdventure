80f9b47637940f791221286a1e87b259f5c2d85f
Kyle Conibear <kyle@gamepill.com>
2020-12-01 20:24:07 -0500

    Creating projectile

---
+++ Unity/Assets/Projectile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float force = 10.0f;
+    [SerializeField] private float lifeDuration = 3.0f;
+    private Rigidbody rigidbody = null;    
+
+    private void Awake()
+    {
+        this.rigidbody = this.GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(DestroySelf());
+    }
+
+    private void FixedUpdate()
+    {
+        this.ApplyForce();
+    }
+
+    private void ApplyForce()
+    {
+        this.rigidbody.AddRelativeForce(Vector3.forward * this.force);
+    }
+
+    private IEnumerator DestroySelf()
+    {
+        yield return new WaitForSeconds(lifeDuration);
+        Destroy(this.gameObject);
+    }
+}
